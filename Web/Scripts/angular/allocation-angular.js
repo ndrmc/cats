f2eec37418dc4ff684ccfb4db8649bb55af7940f
robel alazar <robel_alazar@yahoo.com>
2016-02-24 12:07:05 +0300

    CATS-1217

--- Web/Scripts/angular/allocation-angular.js
+++ Web/Scripts/angular/allocation-angular.js
@@ -56,10 +56,11 @@ app.factory("savefactory", function ($http) {
 
 app.controller("DragDroController", function ($scope, dragDropService, savefactory)
 {
-
+$scope.showModal = false;
     $scope.handleDrop = function (index) {
-       
-        $scope.allocated[0].HubId = index; 
+        
+        $scope.allocated[0].HubId = index;
+        $scope.showModal = !$scope.showModal;
     };
     
     $scope.saveAllocation = function () {
@@ -189,5 +190,45 @@ app.directive('droppable', function () {
     };
 });
 
+app.directive('modal', function () {
+    return {
+        template: '<div class="modal fade">' +
+            '<div class="modal-dialog">' +
+              '<div class="modal-content">' +
+                '<div class="modal-header">' +
+                  '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' +
+                  '<h4 class="modal-title">{{ title }}</h4>' +
+                '</div>' +
+                '<div class="modal-body" ng-transclude></div>' +
+              '</div>' +
+            '</div>' +
+          '</div>',
+        restrict: 'E',
+        transclude: true,
+        replace: true,
+        scope: true,
+        link: function postLink(scope, element, attrs) {
+            scope.title = attrs.title;
 
+            scope.$watch(attrs.visible, function (value) {
+                if (value == true)
+                    $(element).modal('show');
+                else
+                    $(element).modal('hide');
+            });
+
+            $(element).on('shown.bs.modal', function () {
+                scope.$apply(function () {
+                    scope.$parent[attrs.visible] = true;
+                });
+            });
+
+            $(element).on('hidden.bs.modal', function () {
+                scope.$apply(function () {
+                    scope.$parent[attrs.visible] = false;
+                });
+            });
+        }
+    };
+});
 
