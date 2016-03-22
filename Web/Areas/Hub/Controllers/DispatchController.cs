1ab3ed776e151f6587b6129afb573b556419bec2
“Robel <robelaaa@gmail.com>
2016-03-22 14:24:16 -0700

    all

--- Web/Areas/Hub/Controllers/DispatchController.cs
+++ Web/Areas/Hub/Controllers/DispatchController.cs
@@ -392,6 +392,10 @@ namespace Cats.Areas.Hub.Controllers
         public ActionResult CreateDispatch(DispatchViewModel dispatchviewmodel)
         {
             ViewBag.UnitID = new SelectList(_unitService.GetAllUnit(), "UnitID", "Name", dispatchviewmodel.UnitID);
+            var errors = ModelState
+    .Where(x => x.Value.Errors.Count > 0)
+    .Select(x => new { x.Key, x.Value.Errors })
+    .ToArray();
             if (ModelState.IsValid)
             {
 
