31fb5d2d5af8f858a3c66a7eaf39d897098a1460
“Robel <robelaaa@gmail.com>
2016-04-14 10:25:18 -0700

    CATS-1234

--- Services/Cats.Services.Security/SecurityConstants.cs
+++ Services/Cats.Services.Security/SecurityConstants.cs
@@ -35,18 +35,7 @@ namespace Cats.Security
             }
             throw new System.ArgumentException("Unknown Role name", "role");
         }
-        public  string isValidOperation(LogisticsConstants.Operation operation)
-        {
-            var constants = new LogisticsConstants();
-            string acces = "";
-            var ewCache 
-             = (UserPermissionCache)HttpContext.Current.Session["LOGISTICS_PERMISSIONS"];
-            if (ewCache.CheckAccess(constants.ItemName(operation), DateTime.Now) == AuthorizationType.Allow)
-            {
-                acces = operation.ToString();
-            }
-            return acces;
-        }
+        
         /// <summary>
         /// Retrieve Item name from a Task Enum.
         /// </summary>
@@ -1315,7 +1304,20 @@ namespace Cats.Security
 
              #endregion
          }
-
+        public static string isValidOperation(LogisticsConstants.Operation operation)
+        {
+            var constants = new LogisticsConstants();
+            string acces = "";
+            var ewCache
+             = (UserPermissionCache)HttpContext.Current.Session["LOGISTICS_PERMISSIONS"];
+            if (ewCache == null)
+                return "";
+            if (ewCache.CheckAccess(constants.ItemName(operation), DateTime.Now) == AuthorizationType.Allow)
+            {
+                acces = operation.ToString();
+            }
+            return acces;
+        }
         #region Enums
         /// <summary>
         /// Operations Enumeration
