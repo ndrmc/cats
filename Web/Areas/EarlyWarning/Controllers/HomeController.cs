e888f51c8119c4f580b2a288e13ab9474ef1fcd5
nigussemani <nigus.semani@gmail.com>
2016-05-19 12:04:21 -0700

    CATS-1281 has beeen modified.

--- Web/Areas/EarlyWarning/Controllers/HomeController.cs
+++ Web/Areas/EarlyWarning/Controllers/HomeController.cs
@@ -4,6 +4,8 @@ using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Cats.Areas.EarlyWarning.Models;
+using Cats.Services.Dashboard;
 using Cats.Services.EarlyWarning;
 using Cats.Services.Security;
 using Cats.ViewModelBinder;
@@ -19,15 +21,18 @@ namespace Cats.Areas.EarlyWarning.Controllers
         private readonly IRationDetailService _rationDetailService;
         private readonly IRegionalRequestService _regionalRequestService;
         private IUserAccountService _userAccountService;
+        private readonly IEWDashboardService _eWDashboardService;
 
         public HomeController(IHRDService hrdService, IHRDDetailService hrdDetailService,
-                              IRationDetailService rationDetailService, IRegionalRequestService regionalRequestService, IUserAccountService userAccountService)
+            IRationDetailService rationDetailService, IRegionalRequestService regionalRequestService,
+            IUserAccountService userAccountService, IEWDashboardService ewDashboardService)
         {
             _hrdService = hrdService;
             _hrdDetailService = hrdDetailService;
             _rationDetailService = rationDetailService;
             _regionalRequestService = regionalRequestService;
             _userAccountService = userAccountService;
+            _eWDashboardService = ewDashboardService;
         }
 
         public ActionResult Index()
@@ -42,23 +47,30 @@ namespace Cats.Areas.EarlyWarning.Controllers
             ViewBag.PlanID = hrd.PlanID;
             ViewBag.HRDName = hrd.Plan.PlanName;
             var summary = GetHRDSummary(hrd.HRDID);
+            var rounds = GetRounds();
+            if (rounds != null) ViewBag.Rounds = rounds;
+            else ViewBag.Rounds = new List<int>();
+
             return View(summary);
         }
+
         public ActionResult EWMaps()
         {
             return View();
         }
+
         private DataTable GetHRDSummary(int id)
         {
             var weightPref = _userAccountService.GetUserInfo(HttpContext.User.Identity.Name).PreferedWeightMeasurment;
             var hrd = _hrdService.FindById(id);
             var hrdDetails =
                 _hrdDetailService.Get(t => t.HRDID == id, null,
-                                      "AdminUnit,AdminUnit.AdminUnit2,AdminUnit.AdminUnit2.AdminUnit2").ToList();
+                    "AdminUnit,AdminUnit.AdminUnit2,AdminUnit.AdminUnit2.AdminUnit2").ToList();
             var rationDetails = _rationDetailService.Get(t => t.RationID == hrd.RationID, null, "Commodity");
             var dt = HRDViewModelBinder.TransposeDataSummary(hrdDetails, rationDetails, weightPref);
             return dt;
         }
+
         public ActionResult HRDSummaryJson()
         {
             //ModelState.AddModelError("Success", "Sample Error Message. Use in Your Controller: ModelState.AddModelError('Errors', 'Your Error Message.')");
@@ -67,11 +79,25 @@ namespace Cats.Areas.EarlyWarning.Controllers
             {
                 return HttpNotFound();
             }
-           // ViewBag.PlanID = hrd.PlanID;
-           // ViewBag.HRDName = hrd.Plan.PlanName;
+            // ViewBag.PlanID = hrd.PlanID;
+            // ViewBag.HRDName = hrd.Plan.PlanName;
             var summary = GetHRDSummary(hrd.HRDID);
             return Json(summary, JsonRequestBehavior.AllowGet);
         }
-        
+
+        public List<int?> GetRounds()
+        {
+            var currentHrd = _eWDashboardService.FindByHrd(m => m.Status == 3).FirstOrDefault();
+            var requests =
+                _eWDashboardService.FindByRequest(m => m.PlanID == currentHrd.PlanID)
+                    .OrderByDescending(m => m.RegionalRequestID);
+            var reliefRequisitions = _eWDashboardService.GetAllReliefRequisition();
+            return (from reliefRequisition in reliefRequisitions
+                from request in requests
+                where reliefRequisition.RegionalRequestID == request.RegionalRequestID
+                      && reliefRequisition.Round.HasValue
+                select reliefRequisition.Round).Distinct().ToList();
+        }
     }
 }
+
