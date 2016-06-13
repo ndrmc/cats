286c9c9ca1f3d6e6c03fe43a52fd99e57eb11ef7
nathnael <contact.nathnael@gmail.com>
2016-06-13 10:18:54 +0300

    Committing merge files

---
+++ Models/Cats.Models/ViewModels/DashboardDispAlloRequisition.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cats.Models.ViewModels
+{
+    public class DashboardDispAlloRequisition
+    {
+        public int RegionId { get; set; }
+
+        public string RegionName { get; set; }
+
+        public string HrdName { get; set; }
+
+        public int NumberOfEstimatedRequisitions { get; set; }
+
+        public int NumberOfApprovedRequisitions { get; set; }
+
+        public int NumberOfHubAllocatedRequisitions { get; set; }
+
+        public int NumberOfSipcAlloRequisitions { get; set; }
+
+        public int NumberOfCommitedRequisitions { get; set; }
+
+
+    }
+}
