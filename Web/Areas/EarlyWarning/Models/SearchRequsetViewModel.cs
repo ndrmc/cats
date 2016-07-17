2bff48200e68bf4e1d13a382683290cf5fb9ef76
nathnael <contact.nathnael@gmail.com>
2016-07-17 18:48:34 +0300

    HTFX-1243 - CATS-1243 - Inconsistent date datatype on the filter form of the request index - is fixed

--- Web/Areas/EarlyWarning/Models/SearchRequsetViewModel.cs
+++ Web/Areas/EarlyWarning/Models/SearchRequsetViewModel.cs
@@ -10,10 +10,14 @@ namespace Cats.Areas.EarlyWarning.Models
         public SearchRequsetViewModel()
         {
             this.DateTo = DateTime.Now;
+            this.DateToFormatted = String.Format("{0:MM/dd/yyyy}", DateTo);
             this.DateFrom = DateTime.Now.AddMonths(-2);
+            this.DateFromFormatted = String.Format("{0:MM/dd/yyyy}", DateFrom);
         }
         public DateTime DateFrom { get; set; }
+        public string DateFromFormatted { get; set; }
         public DateTime DateTo { get; set; }
+        public string DateToFormatted { get; set; }
         public int? RegionID{ get; set; }
         public int? ProgramID { get; set; }
         public int? StatusID { get; set; }
