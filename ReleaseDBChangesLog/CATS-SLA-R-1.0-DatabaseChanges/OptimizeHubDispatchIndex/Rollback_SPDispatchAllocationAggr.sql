8bed1f5268a46b3ad170f874cf4279baf0025922
nathnael <contact.nathnael@gmail.com>
2016-08-03 12:32:38 +0300

    H.1.0 Optimize Hub Dispatch Index: Used to take forever to load this page because it was trying to aggregate the dispatch data on object level, a lot of nested forloops which is a hickup to system performance. Now its fixed using database views and it will only take under 8 seconds

---
+++ ReleaseDBChangesLog/CATS-SLA-R-1.0-DatabaseChanges/OptimizeHubDispatchIndex/Rollback_SPDispatchAllocationAggr.sql
@@ -0,0 +1,8 @@
+USE [CatsDRMFSS]
+GO
+
+/****** Object:  StoredProcedure [dbo].[SPDispatchAllocationAggr]    Script Date: 8/3/2016 12:27:45 PM ******/
+DROP PROCEDURE [dbo].[SPDispatchAllocationAggr]
+GO
+
+
