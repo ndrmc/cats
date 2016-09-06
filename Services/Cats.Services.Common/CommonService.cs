9755a78cac87dd25ab6c743c3ec58ef1b726eb67
nigussemani <nigus.semani@gmail.com>
2016-09-06 16:15:26 -0700

    Woreda stock distribution data on regional dashboard is fixed to show the sum of all woredas beneficairies for the current region.

--- Services/Cats.Services.Common/CommonService.cs
+++ Services/Cats.Services.Common/CommonService.cs
@@ -151,12 +151,15 @@ namespace Cats.Services.Common
         {
             if (program == (int) Cats.Models.Constant.Programs.Releif)
             {
-                var result =
-                    _unitOfWork.ReliefRequisitionDetailRepository.FindBy(
-                        p =>
-                        p.ReliefRequisition.RegionalRequest.PlanID == planId && p.FDP.AdminUnitID == woredaId &&
-                        p.ReliefRequisition.Round == roundOrMonth).GroupBy(u=>u.RequisitionID).FirstOrDefault().Sum(u=>u.BenficiaryNo);
-  
+                
+                var firstOrDefault =
+                                  _unitOfWork.ReliefRequisitionDetailRepository.FindBy(
+                                      p =>
+                                      p.ReliefRequisition.RegionalRequest.PlanID == planId && p.FDP.AdminUnitID == woredaId &&
+                                      p.ReliefRequisition.Round == roundOrMonth).GroupBy(u => u.RequisitionID).FirstOrDefault();
+
+                var result = firstOrDefault?.Sum(u => u.BenficiaryNo) ?? 0;
+
                 return result;
             }
             if (program == (int) Cats.Models.Constant.Programs.PSNP)
