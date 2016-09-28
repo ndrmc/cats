db32a4ff17245dcb55c062f1ec087a4d64f0b73a
nigussemani <nigus.semani@gmail.com>
2016-09-28 14:23:43 -0700

    Workflow sripts for CATS-1315,CATS-1325, CATS-1331 and CATS-1329 are added

---
+++ ReleaseDBChangesLog/CATS-SLA-R-1.3.1-DatabaseChanges/Other Workflow scripts/CATS-1329/workflow-CATS-1329-Create.sql
@@ -0,0 +1,87 @@
+--Create CATS-1315
+--- Created BY Robel Alazar
+
+use 
+CatsMaster
+go
+IF  NOT EXISTS (
+  SELECT * 
+  FROM   sys.columns 
+  WHERE  object_id = OBJECT_ID(N'[dbo].[LoanReciptPlan]') 
+         AND name = 'BusinessProcessID'
+)
+ --check if BusinessProcessID exist
+ BEGIN
+alter table 
+[dbo].[LoanReciptPlan]
+add   [BusinessProcessID] [int] Default 0 NOT NULL
+ END
+
+declare @ProcessTemplateID table (id int);
+IF Not EXISTS (SELECT * FROM [dbo].[ProcessTemplate] WHERE [Name] = 'Recipt Plan For Loan')
+BEGIN
+  
+   INSERT INTO [dbo].[ProcessTemplate]
+           ([Name]
+           ,[Description]
+           ) Output Inserted.ProcessTemplateID INTO @ProcessTemplateID
+     VALUES
+           ('Recipt Plan For Loan',
+            'Work flow for Recipt Plan For Loan'
+		   )
+		declare @id int  
+		declare @draft int 
+		declare @Approve int 
+		declare @signed int 
+		declare @closed int  
+		select @id = Scope_Identity()  
+		 INSERT INTO [dbo].[ApplicationSetting]
+           ([SettingName]
+           ,[SettingValue]
+           )
+     VALUES
+           ('ReciptPlanForLoanWorkflow'
+           ,@id
+           )
+	INSERT INTO [dbo].[StateTemplate]
+           ([ParentProcessTemplateID] ,[Name],[AllowedAccessLevel],[StateNo]
+           ,[StateType]
+           )
+     VALUES
+           ( @id,'Draft',0,0,0 )  select @draft = Scope_Identity()
+		   INSERT INTO [dbo].[StateTemplate]
+           ([ParentProcessTemplateID] ,[Name],[AllowedAccessLevel],[StateNo]
+           ,[StateType]
+           )
+     VALUES
+		   ( @id,'Approved',0,1,1 )  select @Approve = Scope_Identity()
+		   		   
+		    INSERT INTO [dbo].[StateTemplate]
+           ([ParentProcessTemplateID] ,[Name],[AllowedAccessLevel],[StateNo]
+           ,[StateType]
+           )
+     VALUES
+		   ( @id,'Signed',0,1,1 )  select @signed = Scope_Identity()   
+     INSERT INTO [dbo].[StateTemplate]
+           ([ParentProcessTemplateID] ,[Name],[AllowedAccessLevel],[StateNo]
+           ,[StateType]
+           )
+     VALUES
+		   ( @id,'Closed',0,1,1 )  select @closed = Scope_Identity() 
+
+		   INSERT INTO [dbo].[FlowTemplate]
+           ([ParentProcessTemplateID]
+           ,[InitialStateID]
+           ,[FinalStateID]
+           ,[Name]
+           )
+     VALUES
+           (@id ,@draft,@Approve,'Approve'),
+		    (@id ,@Approve,@signed,'Sign'),
+		   (@id ,@signed,@closed,'Close')
+		   
+		
+		  
+END
+GO
+GO
