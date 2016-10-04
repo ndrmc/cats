bd28730e33252181bc31fdab7ff1aebff4e06893
Yonathan <Yonathanai99@gmail.com>
2016-10-04 15:26:03 +0300

    CATS-1314: workflow script change update

--- ReleaseDBChangesLog/CATS-SLA-R-1.3.1-DatabaseChanges/Stored Procedure - Define Document Workflow for GiftCertificate.sql
+++ ReleaseDBChangesLog/CATS-SLA-R-1.3.1-DatabaseChanges/Stored Procedure - Define Document Workflow for GiftCertificate.sql
@@ -1,11 +1,10 @@
   --Execute the above stored procedure
     DECLARE @h int
     EXECUTE sp_xml_preparedocument @h OUTPUT, N'<Data> 
-    <ProcessTemplate Name="Gift Certificate" Description="Workflow for Gift Certificate" GraphicsData="NULL" PartitionId="0" />
+    <ProcessTemplate Name="GiftCertificate" Description="Workflow for Gift Certificate" GraphicsData="NULL" PartitionId="0" />
     <ApplicationSetting SettingName="GiftCertificateWorkflow" SettingValue="" />
     <StateTemplate Name="Draft" AllowedAccessLevel="0" StateNo="0" StateType="0" FinalStates="Approved" Actions="Approve" />
-    <StateTemplate Name="Approved" AllowedAccessLevel="0" StateNo="1" StateType="1" FinalStates="Rejected,Printed" Actions="Reject,Print" /> 
+    <StateTemplate Name="Approved" AllowedAccessLevel="0" StateNo="1" StateType="1" FinalStates="Rejected" Actions="Reject" /> 
     <StateTemplate Name="Rejected" AllowedAccessLevel="0" StateNo="2" StateType="1" FinalStates="Approved" Actions="Approve" />
-    <StateTemplate Name="Printed" AllowedAccessLevel="0" StateNo="2" StateType="1" FinalStates="Printed" Actions="Print" />
     </Data>'
     EXECUTE define_document_workflow @h
