9b90818f745d12b4cc4ba6d9597ac1a729abb81c
Yonathan <Yonathanai99@gmail.com>
2016-11-03 11:13:31 +0300

    CATS-1372: database change scripts for creating additional columns on ReliefRequisition

---
+++ ReleaseDBChangesLog/CATS-SLA-R-1.3.4-DatabaseChanges/CATS-1372 Adds IsTransfer & TransferId Into ReliefRequisition - Create.sql
@@ -0,0 +1,14 @@
+use CatsDrmfss
+go
+
+ALTER TABLE  Earlywarning.ReliefRequisition
+ADD IsTransfer bit NULL
+go
+
+ALTER TABLE  Earlywarning.ReliefRequisition
+ADD TransferId int NULL
+go
+
+Update EarlyWarning.ReliefRequisition
+Set IsTransfer = 0
+Go
