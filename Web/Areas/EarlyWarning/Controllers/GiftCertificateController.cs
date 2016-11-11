9cf3df15e398245af08e6b80a580d8d5f638e074
Yonathan <Yonathanai99@gmail.com>
2016-11-11 15:55:43 +0300

    CATS-Patch: Old Status field has been replaced with workflow state

--- Web/Areas/EarlyWarning/Controllers/GiftCertificateController.cs
+++ Web/Areas/EarlyWarning/Controllers/GiftCertificateController.cs
@@ -174,7 +174,7 @@ namespace Cats.Areas.EarlyWarning.Controllers
                         {
                             var giftCertificate =
                                 GiftCertificateViewModelBinder.BindGiftCertificate(giftcertificateViewModel);
-                            giftCertificate.StatusID = 1;
+                            giftCertificate.BusinessProcess.CurrentState.BaseStateTemplate.Name = "Draft";
                             giftCertificate.BusinessProcessID = bp.BusinessProcessID;
 
                             var shippingInstructionID =
