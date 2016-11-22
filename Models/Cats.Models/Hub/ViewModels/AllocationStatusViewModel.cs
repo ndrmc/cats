aa6e5710f188400cc24efc77fd813b9597f1cfa2
Yonathan <Yonathanai99@gmail.com>
2016-11-22 19:18:19 +0300

    CATS-1389: workflow implementation for Receive Hub, implementation touches many places.

--- Models/Cats.Models/Hub/ViewModels/AllocationStatusViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Cats.Models.Hubs.ViewModels
-{
-    public class AllocationStatusViewModel
-    {
-        public decimal TotalAllocation { get; set; }
-        public decimal RemainingAllocation { get; set; }
-        public decimal ReceivedAllocation { get; set; }
-    }
-}
--- Models/Cats.Models/Hub/ViewModels/Common/UnitViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Cats.Models.Hubs.ViewModels.Common
-{
-    public class UnitViewModel
-    {
-        public int UnitId { get; set; }
-        public string Name { get; set; }
-    }
-}
