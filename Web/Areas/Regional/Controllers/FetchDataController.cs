0783086de3157103efc8f4a6894847f0deed7a4f
Biruk Tilahun <engbiruk@gmail.com>
2016-12-07 17:18:13 +0300

    Minor Fixes on UI branch

--- Web/Areas/Regional/Controllers/FetchDataController.cs
+++ Web/Areas/Regional/Controllers/FetchDataController.cs
@@ -111,7 +111,6 @@ namespace Cats.Areas.Regional.Controllers
             var utilizations =
                 _utilization.FindBy(
                     t => t.PlanID == currentPlan && t.AdminUnit.AdminUnit2.AdminUnit2.AdminUnitID == regionID);
-            var planName = _planService.FindById(currentPlan).PlanName;
                  
 
             var sum18 = 0;
