8d5df08a6331e30a7f154ed759596079917bab72
unknown <User@WFPETADDAF361.global.wfp.org>
2017-01-09 17:40:38 +0300

    changed forlder name lookups to CatsAPI

---
+++ CatsAPI/Models/Commodity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cats.Rest.Models
+{
+    public class Commodity
+    {
+
+        public int CommodityID { get; set; }
+        public string Name { get; set; }
+        public string LongName { get; set; }
+        public string NameAM { get; set; }
+        public string CommodityCode { get; set; }
+        public int CommodityTypeID { get; set; }
+        public string CommodityTypeName { get; set; }
+        public Nullable<int> ParentID { get; set; }
+        public string ParentCommodityName { get; set; }
+
+        public Commodity(int _commodityId, string _name, string _longName, string _NameAM, string _commodityCode, int _commodtyTypeId, string _commodityTypeName, int? _parentId, string _parentCommodityName)
+        {
+            CommodityID = _commodityId;
+            Name = _name;
+            LongName = _longName;
+            NameAM = _NameAM;
+            CommodityTypeID = _commodtyTypeId;
+            CommodityTypeName = _commodityTypeName;
+            ParentID = _parentId;
+            ParentCommodityName = _parentCommodityName;
+
+
+        }
+    }
+}
