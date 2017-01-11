f0f360f7f85711bfb31a75988463b6da400225e5
Yonathan <Yonathanai99@gmail.com>
2017-01-11 15:09:04 +0300

    CATS-1484: RationDetail REST development

---
+++ CatsAPI/Controllers/RationDetailController.cs
@@ -0,0 +1,64 @@
+﻿using Cats.Services.EarlyWarning;
+using System.Linq;
+using System.Web.Http;
+
+namespace Cats.Rest.Controllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class RationDetailController : ApiController
+    {
+        private readonly IRationDetailService _irationDetailService;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="irationDetailService"></param>
+        public RationDetailController(IRationDetailService irationDetailService)
+        {
+            _irationDetailService = irationDetailService;
+        }
+        /// <summary>
+        /// Returns list of RagionDetail objects
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public dynamic GetRagionDetails()
+        {
+            var bids = _irationDetailService.GetAllRationDetail().Select(item => new
+            {
+                item.RationDetailID,
+                item.RationID,
+                item.CommodityID,
+                CommodityName = item.Commodity.Name,
+                item.Amount,
+                item.UnitID,
+                UnitName = item.Unit.Name
+            }).ToList();
+
+            return bids;
+        }
+        /// <summary>
+        /// Given an id returns a RagionDetail object
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public dynamic GetRagionDetail(int id)
+        {
+            var obj = _irationDetailService.FindById(id);
+            var element = new
+            {
+                obj.RationDetailID,
+                obj.RationID,
+                obj.CommodityID,
+                CommodityName = obj.Commodity.Name,
+                obj.Amount,
+                obj.UnitID,
+                UnitName = obj.Unit.Name
+            };
+
+            return element;
+        }
+    }
+}
