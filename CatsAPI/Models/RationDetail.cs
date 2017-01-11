b84f4b5581491e2ac7f078b47e6341adda2c3d19
Dawit Demissie <dawitdb@yahoo.com>
2017-01-11 14:14:58 +0300

    Add three models

---
+++ CatsAPI/Models/RationDetail.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cats.Rest.Models
+{
+    public class RationDetail
+    {
+        public int RationDetailID { get; set; }
+        public int RationID { get; set; }
+        public int CommodityID { get; set; }
+        public string CommodityName { get; set; }
+        public decimal Amount { get; set; }
+        public Nullable<int> UnitID { get; set; }
+        public string UnitName { get; set; }
+
+        public RationDetail(int rationDetailId, int rationId, int commodityId, string commodityName, decimal amount,
+                            int unitId, string unitName)
+        {
+            RationDetailID = rationDetailId;
+            RationID = rationId;
+            CommodityID = commodityId;
+            CommodityName = commodityName;
+            Amount = amount;
+            UnitID = unitId;
+            UnitName = unitName;
+        }
+
+    }
+}
---
+++ CatsAPI/Models/ReliefRequisitionDetail.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cats.Rest.Models
+{
+    public class ReliefRequisitionDetail
+    {
+        public int RequisitionDetailID { get; set; }
+        public int RequisitionID { get; set; }
+        public int CommodityID { get; set; }
+        public string CommodityName { get; set; }
+        public int BenficiaryNo { get; set; }
+        public decimal Amount { get; set; }
+        public int FDPID { get; set; }
+        public string FDPName { get; set; }
+        public Nullable<int> DonorID { get; set; }
+        public string   DonorName { get; set; }
+        public decimal? Contingency { get; set; }
+
+        public ReliefRequisitionDetail(int requisitionDetailId, int requisitionId, int commodityId, string commodityName, int beneficiaryNo,decimal amount, int fdpId, string fdpName, int donorId, string donorNamae,decimal? contigency)
+        {
+            RequisitionDetailID = requisitionDetailId;
+            RequisitionID = requisitionId;
+            CommodityID = commodityId;
+            CommodityName = commodityName;
+            BenficiaryNo = beneficiaryNo;
+            Amount = amount;
+            FDPID = fdpId;
+            FDPName = fdpName;
+            DonorID = donorId;
+            DonorName = DonorName;
+            Contingency = contigency;
+        }
+    }
+}
