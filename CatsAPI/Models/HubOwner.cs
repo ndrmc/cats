56f0179905ee946e5061b72096f1f6bdfbbe9c51
nigussemani <nigus.semani@gmail.com>
2017-01-11 02:34:50 -0800

    Hub owener api service is added.

---
+++ CatsAPI/Models/HubOwner.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cats.Rest.Models
+{
+    public class HubOwner
+    {
+        public int HubOwnerID { get; set; }
+        public string Name { get; set; }
+        public string LongName { get; set; }
+    }
+}
