55fe63c7394d3afee836cf8b841f3379cddfe73a
Yonathan <Yonathanai99@gmail.com>
2017-02-03 16:05:06 +0300

    TransportRequisitionDetail

--- CatsAPI/App_Start/NinjectWebCommon.cs
+++ CatsAPI/App_Start/NinjectWebCommon.cs
@@ -149,13 +149,14 @@ namespace Cats.Rest.App_Start
             kernel.Bind<ISupportTypeService>().To<SupportTypeService>();
             kernel.Bind<IIDPSReasonTypeServices>().To<IDPSReasonTypeServices>();
 
-            kernel.Bind<Cats.Services.Transaction.ITranscationTypeService>().To<Cats.Services.Transaction.TranscationTypeService>();
+            kernel.Bind<Services.Transaction.ITranscationTypeService>().To<Cats.Services.Transaction.TranscationTypeService>();
             kernel.Bind<Services.Hub.IUserHubService>().To<Services.Hub.UserHubService>();
             kernel.Bind<ITransportBidQuotationHeaderService>().To<TransportBidQuotationHeaderService>();
             kernel.Bind<IRegionalPSNPPlanService>().To<RegionalPSNPPlanService>();
             kernel.Bind<ITransportOrderService>().To<TransportOrderService>();
             kernel.Bind<ITransportOrderDetailService>().To<TransportOrderDetailService>();
             kernel.Bind<ITransportRequisitionService>().To<TransportRequisitionService>();
+            kernel.Bind<ITransportRequisitionDetailService>().To<TransportRequisitionDetailService>();
         }
 
     }
---
+++ Services/Cats.Services.Logistics/TransportRequisitionDetailService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Cats.Data.UnitWork;
+using Cats.Models;
+using Cats.Models.Constant;
+using Cats.Models.ViewModels;
+using Cats.Services.Logistics;
+using Cats.Services.Common;
+
+namespace Cats.Services.Logistics
+{
+    public class TransportRequisitionDetailService : ITransportRequisitionDetailService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly INotificationService _notificationService;
+     
+
+        public TransportRequisitionDetailService(IUnitOfWork unitOfWork, INotificationService notificationService)
+        {
+            this._unitOfWork = unitOfWork;
+            _notificationService = notificationService;
+        }
+
+        #region Default Service Implementation
+
+        public List<TransportRequisitionDetail> GetAllTransportRequisitionDetail()
+        {
+            return _unitOfWork.TransportRequisitionDetailRepository.GetAll();
+        }
+        public TransportRequisitionDetail FindById(int id)
+        {
+            return _unitOfWork.TransportRequisitionDetailRepository.FindById(id);
+        }
+     
+        #endregion
+        
+        public void Dispose()
+        {
+            _unitOfWork.Dispose();
+
+        }        
+    }
+}
+
+
