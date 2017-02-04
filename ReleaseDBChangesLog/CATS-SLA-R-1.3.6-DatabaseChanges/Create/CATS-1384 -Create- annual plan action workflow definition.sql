707c8cf5d3b446bb2b8dee784743c38dd07c4495
nigussemani <nigus.semani@gmail.com>
2017-02-04 11:47:36 +0300

    CATS-1381 , CATS-1384 , CATS-1388 and CATS-1402 Workflow action script is added.

---
+++ ReleaseDBChangesLog/CATS-SLA-R-1.3.6-DatabaseChanges/Create/CATS-1384 -Create- annual plan action workflow definition.sql
@@ -0,0 +1,16 @@
+
+
+DECLARE @h int
+EXECUTE sp_xml_preparedocument @h OUTPUT, N'<Data>  
+	<ProcessTemplate Name="PSNP Plan" Description="Plan to Execution" GraphicsData="NULL" PartitionId="0" />
+	<ApplicationSetting SettingName="PSNPWorkflow" SettingValue="" />
+	<StateTemplate Name="Draft" AllowedAccessLevel="0" StateNo="0" StateType="0" FinalStates="Edited,Submitted for Approval" Actions="Edit,Ask Approval" />
+	<StateTemplate Name="Submitted for Approval" AllowedAccessLevel="0" StateNo="1" StateType="1" FinalStates="Under Revision,Approved" Actions="Request Revision,Approve" /> 
+	<StateTemplate Name="Under Revision" AllowedAccessLevel="0" StateNo="2" StateType="1" FinalStates="Edited,Submitted for Approval" Actions="Edit,Ask Approval," />
+	<StateTemplate Name="Approved" AllowedAccessLevel="0" StateNo="3" StateType="1" FinalStates="Completed" Actions="Completed" />
+	<StateTemplate Name="Completed" AllowedAccessLevel="0" StateNo="4" StateType="1" FinalStates="" Actions="" />
+	<StateTemplate Name="Edited" AllowedAccessLevel="0" StateNo="5" StateType="1" FinalStates="Edited,Submitted for Approval" Actions="Edit,Ask Approval" />
+	
+</Data>'
+
+EXECUTE update_action_workflow @h
