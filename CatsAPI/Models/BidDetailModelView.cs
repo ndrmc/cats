0465764201ea2bdf8711c148ad614dc8298a9e19
Yonathan <Yonathanai99@gmail.com>
2017-02-07 22:21:23 +0300

    CATS-1460: update change to BidDetail REST development...

---
+++ CatsAPI/Models/BidDetailModelView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace Cats.Rest.Models
+{
+    public class BidDetailModelView
+    {
+        public int BidDetailId { get; set; }
+        public decimal AmountForReliefProgram { get; set; }
+        public decimal AmountForPSNPProgram { get; set; }
+        public decimal BidDocumentPrice { get; set; }
+        public decimal CPO { get; set; }
+    }
+}
---
+++ CatsAPI/Models/BidModelView.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace Cats.Rest.Models
+{
+    public class BidModelView
+    {
+        public int BidID { get; set; }
+        public int RegionID { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string BidNumber { get; set; }
+        public DateTime OpeningDate { get; set; }
+        public int StatusID { get; set; }
+        public string Status { get; set; }
+        public int TransportBidPlanID { get; set; }
+        public decimal BidBondAmount { get; set; }
+        public string startTime { get; set; }
+        public string endTime { get; set; }
+        public string BidOpeningTime { get; set; }
+    }
+}
